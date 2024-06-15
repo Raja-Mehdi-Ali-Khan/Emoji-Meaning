2325b1db299fb675c14ae57cad4ac579d16dedef
Raja-Mehdi-Ali-Khan <422244@student.nitandhra.ac.in>
2024-06-15 16:51:27 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import Emoj from "./Emoj";
+import emojipedia from "../emojipedia";
+
+function createCard(emojipedia) {
+  return (
+    <Emoj
+      key={emojipedia.id}
+      emoji={emojipedia.emoji}
+      name={emojipedia.name}
+      meaning={emojipedia.meaning}
+    />
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+
+      <dl className="dictionary">
+        {emojipedia.map(createCard)}
+
+        {/* <Emoj
+          emoji={emojipedia[0].emoji}
+          name={emojipedia[0].name}
+          meaning={emojipedia[0].meaning}
+        />
+        <div className="term">
+          <dt>
+            <span className="emoji" role="img" aria-label="Tense Biceps">
+              🙏
+            </span>
+            <span>Person With Folded Hands</span>
+          </dt>
+          <dd>
+            Two hands pressed together. Is currently very introverted, saying a
+            prayer, or hoping for enlightenment. Is also used as a “high five”
+            or to say thank you.
+          </dd>
+        </div>
+        <div className="term">
+          <dt>
+            <span className="emoji" role="img" aria-label="Tense Biceps">
+              🤣
+            </span>
+            <span>Rolling On The Floor, Laughing</span>
+          </dt>
+          <dd>
+            This is funny! A smiley face, rolling on the floor, laughing. The
+            face is laughing boundlessly. The emoji version of “rofl“. Stands
+            for „rolling on the floor, laughing“.
+          </dd>
+        </div> */}
+      </dl>
+    </div>
+  );
+}
+
+export default App;
